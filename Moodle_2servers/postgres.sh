85d9cf78ece9d6da2f1e957fd0593ccd919f3fdc
xala <cristiano@catalyst.net.nz>
2015-06-03 15:07:07 +1200

    HEAT Template file

---
+++ Moodle_2servers/postgres.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -v
+# Add server name to host file
+echo "127.0.0.1 pg-dbserver" >> /etc/hosts
+# Redirect output to syslog
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+# Set timezone
+echo "Pacific/Auckland NZ" | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+# Set up shell variables
+export DEBIAN_FRONTEND=noninteractive
+export SITENAME=site_name
+export ENVIRONMENT=environment
+export APPTYPE=app_type
+export SITEENVIRONMENT=$SITENAME-$ENVIRONMENT-$APPTYPE
+# DATABASE
+# Install PostgreSQL, modify config files, restart and create database and user
+echo 'APT::Install-Recommends "0";' >> /etc/apt/apt.conf.d/90install-recommends
+apt-get update
+apt-get -y install postgresql
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /etc/postgresql/9.3/main/postgresql.conf
+echo "host    $SITEENVIRONMENT $SITEENVIRONMENT             192.168.0.0/24        md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+/etc/init.d/postgresql restart
+sudo -u postgres createuser --no-superuser --no-createdb --no-createrole $SITEENVIRONMENT
+sudo -u postgres psql -c "alter user \"$SITEENVIRONMENT\" with password 'db_rootpassword'"
+sudo -u postgres createdb -Eutf8 --owner=$SITEENVIRONMENT $SITEENVIRONMENT -T template0
+
